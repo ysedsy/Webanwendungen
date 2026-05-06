fc0ab9511810d2e8dd2b1c93a22b589eaa721ed9
thorbenmueller1 <thorben.mue@gmail.com>
2026-05-06 10:44:19 +0200

    minor changes

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const path = require ("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "Frontend")))
 
 const db = new Database(path.join(__dirname, "birdlexicon.db"));
 
