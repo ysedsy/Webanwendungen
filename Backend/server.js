0ef1c5f08e96f1467c5521f973b7f27295a7bd5e
ysedsy <felixgaartz@gmail.com>
2026-05-08 10:28:10 +0200

    fixed swagger

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const Database = require("better-sqlite3");
 const path = require ("path");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("js-yaml");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
@@ -9,6 +12,10 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, "../Frontend")))
 
 const db = new Database(path.join(__dirname, "birdlexicon.db"));
+
+// Load and setup Swagger documentation
+const swaggerDocument = YAML.load(fs.readFileSync(path.join(__dirname, "openapi.yaml"), "utf8"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/api", (req,res) => {
     res.send("Bird Lexicon API is running");
